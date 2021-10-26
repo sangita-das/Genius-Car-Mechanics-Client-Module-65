71cbb421e3218c823c19892863eaa630c9f3ee64
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2021-10-26 12:58:51 +0600

    adding

---
+++ src/Pages/AddService/AddService.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form";
+import './AddService.css'
+
+
+
+const AddService = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    axios.post('http://localhost:5000/services', data)
+      .then(res => {
+        console.log(res)
+
+        if (res.data.insertedId) {
+          alert('added successfully');
+          reset();
+        }
+      })
+  }
+  return (
+    <div className="add-service">
+      <h2>Please Add a service</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("firstName", { required: true, maxLength: 20 })} placeholder="Name" />
+
+        <textarea {...register("description")} placeholder="Description" />
+        <input type="number" {...register("price")} placeholder="Price" />
+        <input {...register("img")} placeholder="image url" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default AddService;
